22c7324a192164ab0bc6d6c7b43ccec6a09262c0
Bryan L.H <spitfire9618@gmail.com>
2016-10-11 00:59:30 -0500

    Ipoo Tarea 11/10/16

---
+++ listasenl.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*struct sname
+{
+    int a;
+    char b;
+};
+
+
+main ()
+{
+    struct sname s;
+    s.a = 5;
+    s.b = 'x';
+}/*
+struct node
+{
+    int val;
+    struct node *next;
+};
+main()
+{
+    struct node *head, *middle, *tail;
+    head = malloc (sizeof(struct node));
+    middle = malloc (sizeof(struct node));
+    tail = malloc (sizeof(struct node));
+    head -> val = 1;
+    middle -> val = 2;
+    tail -> val = 3;
+    head -> next = middle;
+    middle -> next = tail;
+    tail -> next = NULL;
+
+    /*struct node *t;
+    t = head;
+    printf("%d\t", t->val);
+    t = t -> next;
+    printf("%d\t", t->val);
+    t = t -> next;
+    printf("%d\t", t -> val);*/
+
+}
+struct node *crearlista(int n)
+{
+    struct node *head;
+    struct node *prev;
+    struct node *cur;
+    int i;
+    head = malloc (sizeof(struct node));
+    head -> val = 1;
+    prev = head;
+    for (i = 2; i <= n; i++)
+        {
+            cur = malloc (sizeof (struct node));
+            cur -> val = i;
+            prev -> next = cur;
+            prev = cur;
+        }
+        prev -> next = NULL;
+    return head;
+}
+
